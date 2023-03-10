ff79280bd2afcd90e7dcdd9d0fa281920348decb
yenyou <yenyouchen@richitech.com.tw>
2023-03-10 09:18:00 +0800

    新增網頁內容

---
+++ app.js
@@ -0,0 +1,46 @@
+import init, { fibonacci as fibonacci } from "./pkg/hello_wasm.js";
+
+
+const vm = new Vue( {
+    data: {
+        n: 43,
+        ansJs: [],
+        ansWasm: [],
+        timeJs: 0,
+        timeWasm: 0,
+    },
+    methods: {
+        runInJs: runInJs,
+        runInWasm: runInWasm,
+    }
+} );
+
+vm.$mount( '#app' );
+
+function runInJs() {
+    const start = new Date();
+    const ans = fibonacciJs( this.n );
+    this.ansJs.unshift( ans );
+    const end = new Date();
+    this.timeJs = ( end - start ) / 1000;
+}
+
+function fibonacciJs( n ) {
+    switch ( n ) {
+        case 0:
+            return 1;
+        case 1:
+            return 1;
+        default:
+            return fibonacciJs( n - 1 ) + fibonacciJs( n - 2 );
+    }
+}
+
+async function runInWasm() {
+    await init();
+    const start = new Date();
+    const ans = fibonacci( this.n );
+    this.ansWasm.unshift( ans );
+    const end = new Date();
+    this.timeWasm = ( end - start ) / 1000;
+}
---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn fibonacci(n: u32) -> u32 {
+    match n {
+        0 => 1,
+        1 => 1,
+        _ => fibonacci(n - 1) + fibonacci(n - 2),
+    }
+}
